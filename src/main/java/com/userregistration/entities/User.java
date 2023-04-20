f6f8ddc71209e992fa835291a262b9d90a02023a
Daniel de Jesus <danieldejesus8745@gmail.com>
2023-04-19 22:32:32 -0300

    setting up application.yml e creating User entity

---
+++ src/main/java/com/userregistration/entities/User.java
@@ -0,0 +1,28 @@
+package com.userregistration.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+}
